b78d53937ceee99eb6969fcae8c07e91429aa7e9
Misha Dovhiy <hi@mishadovhiy.com>
2025-03-29 22:29:30 +0200

    new assets

--- MatchGame/DataModel/DataType/LvlType.swift
+++ MatchGame/DataModel/DataType/LvlType.swift
@@ -14,30 +14,24 @@ extension LevelModel {
         var allowedAssetNames:[String] {
             switch self {
             case .Meat:
-                var values = ["cheaps", "pasta", "pizza", "popcorn", "pizza"]
-                values.append(contentsOf: (0..<26).compactMap({"bakeryUnhealthy\($0)"}))
-                return values.shuffled()
+                return (0..<13).compactMap({"bakeryUnhealthy\($0)"}).shuffled()
             case .Bakery:
-                return (0..<23).compactMap({"vegs\($0)"}).shuffled()
+                return (0..<10).compactMap({"vegs\($0)"}).shuffled()
             case .DrinksAndFastFood:
-                var values = Array(1...15).compactMap({"fruits\($0)"})
-             //   values.append(contentsOf: (0..<13).compactMap({"fruit\($0)"}))
+                var values = Array(14...21).compactMap({"fruit\($0)"})
+                values.append(contentsOf: (0..<13).compactMap({"ftuit\($0)"}))
                 return values.shuffled()
             case .FruitsANDvegitables:
-                var buttle = ["buttleBlack", "buttleBlue", "buttleBlue2", "buttleBrown", "buttleGreen", "milk1", "milk", "milk2"]
-                buttle.append(contentsOf: (0..<11).compactMap({"water\($0)"}))
-                return buttle.shuffled()
+                return (0..<12).compactMap({"water\($0)"}).shuffled()
             case .fastFood:
-                var values = ["bakeryDonate"]
-                values.append(contentsOf: Array(1...9).compactMap({"bakerySweet\($0)"}))
-                values.append(contentsOf: Array(1...2).compactMap({"bakeryBread\($0)"}))
-                return values.shuffled()
-            case .Drinks://replace
-                return ["buttleBlack", "buttleBlue", "buttleBlue2", "buttleBrown", "bakerySweet1", "bakerySweet2", "bakerySweet3", "bakerySweet4", "buttleGreen", "milk1", "milk", "milk2"]
+                return (14..<26).compactMap({"bakeryUnhealthy\($0)"})
+            case .Drinks:
+                return Array(1...17).compactMap({"fastfood\($0)"})
             case .Milk://replace
-                return ["garlic", "milk1", "milk", "milk2", "eggs", "paper", "buttleGreen", "bakerySweet4", "bakerySweet5", "bakerySweet6", "buttleBlue2", "milk", "cheaps", "pasta"]
+                return (11..<23).compactMap({"vegs\($0)"}).shuffled()
+
             case .MuilkANDcheese://replace
-                return ["garlic", "milk1", "milk", "milk2", "eggs", "paper", "eggs", "bakerySweet6", "bakerySweet7", "bakerySweet8", "cheese", "cheaps", "buttleGreen", "buttleBlue2", "bakerySweet3"]
+                return (13..<26).compactMap({"water\($0)"}).shuffled()
             }
         }
         
